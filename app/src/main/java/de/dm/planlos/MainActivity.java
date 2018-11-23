c3a5e06f1db4e5c9c3c91db5d19577effea9bdf0
Daniel Flassak <daniel.flassak@dm.de>
2018-11-23 15:08:53 +0100

    ask for bluetooth permission

--- app/src/main/java/de/dm/planlos/MainActivity.java
+++ app/src/main/java/de/dm/planlos/MainActivity.java
@@ -1,13 +1,58 @@
 package de.dm.planlos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final String LOG_TAG = "NINa";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("NINa needs to know where you are.");
+                builder.setMessage("Please grant location access so that NINa can tell you and your colleagues where you are.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(dialog -> {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                });
+                builder.show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(LOG_TAG, "coarse location permission granted");
+                }
+                else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited.");
+                    builder.setMessage("Since location access has not been granted, NINa cannot tell you or your colleagues where you are.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener((dialog) -> {
+                        //TODO: do we need to do something here?
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
     }
 }
